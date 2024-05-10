561eac3023ea7441f31c6abbca62fafcd1c8d0eb
Ohno0704 <68172397+aishoku17127@users.noreply.github.com>
2024-05-10 23:20:06 +0900

    NFTアプリデモ完成

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,14 @@ import "@nomicfoundation/hardhat-chai-matchers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  typechain: {
+    outDir: "frontend/types",
+    target: "ethers-v6",
+    alwaysGenerateOverloads: false,
+    // コントラクトにおける関数のオーバーロードがない場合でも"deposit(uint256)のようなシグネチャを生成するか
+    // externalArtifacts: ["externalArtifacts/*.json"],
+    // Typeファイルの生成に追加したい外部のArtifactsがある場合は指定する
+  }
 };
 
 export default config;
